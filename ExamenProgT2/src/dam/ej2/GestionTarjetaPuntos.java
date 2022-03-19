61f9b809b15c66b9ba6dda621be1c30db35a7bb6
baljaudev <r.baljau@gmail.com>
2022-03-19 11:41:48 +0100

    Creando programa puntos Gasolinera

--- ExamenProgT2/src/dam/ej2/GestionTarjetaPuntos.java
+++ ExamenProgT2/src/dam/ej2/GestionTarjetaPuntos.java
@@ -1,2 +1,8 @@
-package dam.ej2;public class GestionTarjetaPuntos {
+package dam.ej2;
+
+import java.util.HashMap;
+
+public class GestionTarjetaPuntos {
+    static HashMap<String, Integer> tablaPuntos;
+
 }
